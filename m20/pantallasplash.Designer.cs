f26c230c3dd79794ba02f4626fa9a302fbf78838
polsadurni <polsadurniaviles@gmail.com>
2021-11-16 19:49:46 +0100

    Revert "dsf"
    This reverts commit 1fdd0cc4921caa714cc30412eee1e8ddfe90e2e1.

--- m20/pantallasplash.Designer.cs
+++ m20/pantallasplash.Designer.cs
@@ -87,7 +87,6 @@ namespace m20
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(644, 372);
