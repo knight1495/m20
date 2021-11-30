7923b992bc881b1018d8277b607db97143b65e66
SPKwarfo <sp.kwarfo@sarria.salesians.cat>
2021-11-30 20:28:36 +0100

    Btn Close de PanelAdmin y TablaCoord

--- m20/PanelAdmin.Designer.cs
+++ m20/PanelAdmin.Designer.cs
@@ -140,6 +140,7 @@ namespace m20
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox2.TabIndex = 8;
             this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
             // 
             // PanelAdmin
             // 
